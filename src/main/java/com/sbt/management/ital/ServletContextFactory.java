d1eb580c43c25deea043cd3e3afc9447a175d0a8
ArchiMage <archimagealex@gmail.com>
2018-01-29 11:00:54 +0300

    Bootstrap correct

--- src/main/java/com/sbt/management/ital/ServletContextFactory.java
+++ src/main/java/com/sbt/management/ital/ServletContextFactory.java
@@ -7,24 +7,19 @@ import org.springframework.web.context.ServletContextAware;
 public class ServletContextFactory implements FactoryBean<ServletContext>, ServletContextAware {
 	private ServletContext servletContext;
 
-	@Override
-	public ServletContext getObject() throws Exception {
-		return servletContext;
-	}
-
-	@Override
-	public Class<?> getObjectType() {
-		return ServletContext.class;
-	}
-
-	@Override
-	public boolean isSingleton() {
-		return true;
-	}
-
-	@Override
 	public void setServletContext(ServletContext servletContext) {
+		// TODO Auto-generated method stub
 		this.servletContext = servletContext;
+	}
+
+	public ServletContext getObject() throws Exception {
+		// TODO Auto-generated method stub
+		return this.servletContext;
+	}
+
+	public Class<?> getObjectType() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
